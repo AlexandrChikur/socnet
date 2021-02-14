d3ce4bc02fbfe5655fcc814713564eb5c441efad
AlexandrChikur <sasha.chikur@mail.ru>
2021-02-15 00:29:52 +0300

    Deleted api info endpoint

--- app/api/routes/api.py
+++ app/api/routes/api.py
@@ -1,7 +1,6 @@
 from typing import Any, Dict
 
-from app.core.config import Settings, get_settings
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter
 
 from .v1 import api as api_v1
 
@@ -10,14 +9,3 @@ router = APIRouter()
 # fmt: off
 router.include_router(api_v1.router, prefix="/v1", tags=["v1"])
 # fmt: on
-
-
-@router.get("/")
-async def api_info(settings: Settings = Depends(get_settings)) -> Dict[str, Any]:
-    """ Endpoint that give an info about API """
-
-    return {
-        "project_name": settings.project_name,
-        "project_description": settings.project_description,
-        "version": settings.version,
-    }
